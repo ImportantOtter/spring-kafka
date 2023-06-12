508de7603ba41286ae41b104b0f9cd455e4fff15
ImportantOtter <importantotter@gmail.com>
2023-06-12 13:55:59 +0300

    update kafka config logic

--- src/main/java/com/example/config/KafkaProducerConfig.java
+++ src/main/java/com/example/config/KafkaProducerConfig.java
@@ -1,10 +1,12 @@
 package com.example.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -35,5 +37,11 @@ public class KafkaProducerConfig {
     public KafkaTemplate<String, String> kafkaTemplate(
             ProducerFactory<String, String> producerFactory) {
         return new KafkaTemplate<>(producerFactory);
+    }
+
+    @Bean
+    public NewTopic newTopic() {
+        return TopicBuilder.name("message")
+                .build();
     }
 }
